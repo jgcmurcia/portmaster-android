57af84df074ca9f6bd862a12d7a3810433b46c07
jvgc <151227523+jgcmurcia@users.noreply.github.com>
2026-09-09 22:02:34 +0200

    Forward underlying network changes to VPN service

--- android/app/src/main/java/io/safing/portmaster/android/connectivity/NetworkCallbacks.java
+++ android/app/src/main/java/io/safing/portmaster/android/connectivity/NetworkCallbacks.java
@@ -10,21 +10,32 @@ import engine.Engine;
 import io.safing.portmaster.android.os.NetworkProxy;
 
 public class NetworkCallbacks extends ConnectivityManager.NetworkCallback {
+  private final PortmasterTunnelService service;
+
+  public NetworkCallbacks(PortmasterTunnelService service) {
+    this.service = service;
+  }
+
   @Override
   public void onAvailable(@NonNull Network network) {
     super.onAvailable(network);
     Engine.onNetworkConnected();
+    service.onUnderlyingNetworkAvailable(network);
   }
 
   @Override
   public void onLost(@NonNull Network network) {
     super.onLost(network);
     Engine.onNetworkDisconnected();
+    service.onUnderlyingNetworkLost(network);
   }
 
   @Override
-  public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+  public void onCapabilitiesChanged(
+      @NonNull Network network,
+      @NonNull NetworkCapabilities networkCapabilities) {
     super.onCapabilitiesChanged(network, networkCapabilities);
     Engine.onNetworkCapabilitiesChanged(new NetworkProxy(networkCapabilities));
+    service.onUnderlyingNetworkCapabilitiesChanged(network, networkCapabilities);
   }
 }
